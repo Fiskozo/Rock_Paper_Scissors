425664c9c0decb8caa33a2c015f090169e6f313e
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2023-11-15 00:11:58 +0100

    Add ComputerChoice, PlayerChoice And PlayRound
    Add function to get a random value from an array as computerchoice.
    Add function to get PlayerChoice
    Add function to play one round

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// Make function to get computers choice
+
+let arr = ["rock", "paper", "scissor"];
+function getComputerChoice(min, max) {
+    return(arr[(Math.floor(Math.random() * arr.length))]);
+    }; 
+
+// Make function to get players choice using prompt()
+ 
+let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+console.log(playerChoice);
+
+// Declare Rock, Paper, Scissors and what beats what
+/* Make function to play one round, using 2 parameters
+(remember to return results) */
+
+function playRound(playerSelection, computerSelection) {
+console.log(computerSelection)
+
+if 
+  (playerSelection === computerSelection){
+  return String("Tie")
+}else if
+  ((playerSelection === "rock") && (computerSelection === "scissor")) {
+  return String("you win1");
+}else if 
+  ((playerSelection === "scissor") && (computerSelection === "paper")){
+    return String("you win2");
+}else if 
+  ((playerSelection === "paper") && (computerSelection === "rock")){
+    return String("you win3");
+}else {
+  return String("LOSER")
+}
+} 
+
+const playerSelection = playerChoice;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+//Make function to play 5 rounds and keep score
