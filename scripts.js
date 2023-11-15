96a89814c2a71d2327af1efe12c16116667e84c8
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2023-11-15 10:27:24 +0100

    Add Count To Keep Count of Wins

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Make function to get computers choice
-
+let count = 0
 let arr = ["rock", "paper", "scissor"];
 function getComputerChoice(min, max) {
     return(arr[(Math.floor(Math.random() * arr.length))]);
@@ -19,24 +19,29 @@ console.log(computerSelection)
 
 if 
   (playerSelection === computerSelection){
-  return String("Tie")
+    return String("Tie")
 }else if
   ((playerSelection === "rock") && (computerSelection === "scissor")) {
-  return String("you win1");
+    count++;
+    return String("you win1");
 }else if 
   ((playerSelection === "scissor") && (computerSelection === "paper")){
+    count++;
     return String("you win2");
 }else if 
   ((playerSelection === "paper") && (computerSelection === "rock")){
+    count++;
     return String("you win3");
 }else {
   return String("LOSER")
 }
 } 
 
+
 const playerSelection = playerChoice;
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
-
+console.log(count)
 
 //Make function to play 5 rounds and keep score
+
