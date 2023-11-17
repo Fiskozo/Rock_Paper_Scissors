015b0139f4b7728079ff79c0d1670f0bed41bd75
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2023-11-17 13:18:31 +0100

    Add Score For Computer

--- scripts.js
+++ scripts.js
@@ -1,47 +1,40 @@
 // Make function to get computers choice
-let count = 0
-let arr = ["rock", "paper", "scissor"];
+
 function getComputerChoice(min, max) {
+    let arr = ["rock", "paper", "scissor"];
     return(arr[(Math.floor(Math.random() * arr.length))]);
     }; 
-
 // Make function to get players choice using prompt()
- 
-let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-console.log(playerChoice);
-
-// Declare Rock, Paper, Scissors and what beats what
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+  return playerChoice;
+}
 /* Make function to play one round, using 2 parameters
 (remember to return results) */
-
+let playerScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
-console.log(computerSelection)
-
 if 
   (playerSelection === computerSelection){
     return String("Tie")
 }else if
   ((playerSelection === "rock") && (computerSelection === "scissor")) {
-    count++;
+    playerScore++;
     return String("you win1");
 }else if 
   ((playerSelection === "scissor") && (computerSelection === "paper")){
-    count++;
+    playerScore++;
     return String("you win2");
 }else if 
   ((playerSelection === "paper") && (computerSelection === "rock")){
-    count++;
+    playerScore++;
     return String("you win3");
 }else {
+  computerScore++;
   return String("LOSER")
 }
 } 
-
-
-const playerSelection = playerChoice;
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(count)
-
+console.log(playRound(playerSelection, computerSelection))
 //Make function to play 5 rounds and keep score
-
